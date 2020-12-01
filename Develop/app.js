249c5cb685c5d950e73f6a3236bea3c8b4bf3bc6
luvwitch <itsalexkelly@gmail.com>
2020-11-30 20:15:18 -0800

    building switch case & questions for each role

--- Develop/app.js
+++ Develop/app.js
@@ -10,72 +10,79 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const questionsArr = 
-[  
-  {
-    type: 'check',
-    name: 'role',
-    message: 'Which role would you like to hire for?',
-    choices: ['Manager','Engineer', 'Intern'],
-  },
-  {
-    type: 'input',
-    message: 'Name of new hire?',
-    name: 'name',
-  },
-  {
-    type: 'input',
-    message: 'Id of new hire?',
-    name: 'id',
-  },
-  {
-    type: 'input',
-    message: 'Email of new hire?',
-    name: 'email',
-  } 
-];
+const employeeArr = [];
 
-const addMore =
-[{
-  type: 'check',
-  name: 'addmore',
-  message: 'Add more team members?',
-  choices: ['yes', 'no'],        
-}];
-
-const employeesArr = "";
-
-createArr() = () => {  
-  inquirer  
-    .prompt(questionsArr);
-    .then((data) =>
-      console.log("New " + choice.role + " hired!"),
-      addArr(),
-    );
+createEmployee = () => {  
+  inquirer.prompt([
+    {
+      type: 'check',
+      name: 'role',
+      message: 'Which role would you like to hire for?',
+     choices: ['Manager','Engineer', 'Intern'],
+    }
+  ]).then(response => {
+    console.log("New " + response.role + " hired!"),
+    switch (response.role){
+      case 'Manager':
+         managerQ();
+         break;
+      case 'Engineer':
+        engineerQ();
+        break;
+      case 'Intern':
+        internQ();
+        break;
+      default:
+        console.log("You must choose a role.");
+        return;  
+    };
+  });
 };
 
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
 
-addArr() = () => { 
-  inquirer
-    .prompt(addMore);
-    .then((data) =>
-      if (choice.addmore === 'yes') {
-        createArr()
+
+managerQ = () => {
+
+};
+
+engineerQ = () => {
+
+};
+
+internQ = () => {
+
+};
+
+addAgain = () => { 
+  inquirer.prompt([
+    {      
+      type: 'check',
+      name: 'add',
+      message: 'Add more team members?',
+      choices: ['yes', 'no'],     
+
+    }
+  ]).then(response => {
+      if (response.add === 'yes') {
+        createEmployee();
         }; 
-      if (choice.addmore === 'no') {
-        render(employeesArr)
-        fs.writeFile('/output/team.html');
+      if (response.add === 'no') {
+        createTeam();
+        console.log("Looks like a good batch of new hires!");
       };
-    );      
+  });      
 };
-
-
-createArr();
-
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+createTeam = () => {
+
+};
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
@@ -83,12 +90,3 @@ createArr();
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
